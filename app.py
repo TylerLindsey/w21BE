a2e1d121ea2aefa82d677bb306e8e75b4e2f3df3
tygoli <tylerlindsey403@gmail.com>
2022-06-14 09:08:03 -0600

    basic implementation of db interaction

--- app.py
+++ app.py
@@ -4,24 +4,35 @@ import sys
 
 app = Flask(__name__)
 
-@app.post('/api/poster')
-def poster_post():
+@app.get('/api/poster')
+def poster_get():
   # TODO DB select
-  poster_list =run_query()
-  # will replace the above at some point
-  return jsonify(poster_list), 200
+  poster_list = run_query("SELECT * FROM poster")
+  resp = []
+  for poster in poster_list:
+    an_obj = {}
+    an_obj['id'] = poster[0]
+    an_obj['posterContent'] = poster[1]
+    resp.append(an_obj)
+  return jsonify(resp), 200
 # poster is what i am calling the data, liike animals in the class example
 @app.post('/api/poster')
 def poster_post():
   data = request.json
   # above isva the object that allows me to access the data from the request
-  poster_name = data.get('posterName')
+  # id = data.get('Id')
+  poster_content = data.get('posterContent')
   # above is the name of variable in the API request
+  # called in the run_query a few lines below here and in the 3d an_obj spot
   # document what exactly the name is (capitalization and whatnot)
   
   # check if the above is set, if the get has no value in the dictionary the data will return none because none is = to false, so use conditional to add an error if the value is none 
-  if not poster_name:
+  
+  # if not id:
+  #   return jsonify('Could not process but understood the request'), 422
+  if not poster_content:
     return jsonify('Could not process but understood the request'), 422
+  run_query("INSERT INTO poster (posterConent) VALUES(?,?)", [poster_content])
   return jsonify('post added'), 201
 
 if len(sys.argv) >1:
